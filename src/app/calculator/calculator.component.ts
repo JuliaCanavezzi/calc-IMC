6cf4d1e61fedaa90422e409809ad7952bd00c7b7
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-04-29 22:20:16 -0300

    calculo imc

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+  imcFormGroup: FormGroup;
+  resultado: number = 0;
+  analiseImc: String = '';
 
+  constructor(private formBuilder: FormBuilder ){
+    this.imcFormGroup = formBuilder.group({
+      peso: [''],
+      altura: [''],
+    });
+  }
+
+  calc(){
+    const peso = this.imcFormGroup.controls['peso'].value;
+    const altura = this.imcFormGroup.controls['altura'].value;
+   const imc = peso / (altura * altura);
+   this.resultado = Number(Number(imc).toFixed(2));
+
+   this.analisaIMC()
+  }
+
+ analisaIMC(){
+  if(this.resultado <= 18.5 ){
+    this.analiseImc = 'baixo peso'
+  } else if(this.resultado > 18.5 && this.resultado <= 24.9 ){
+    this.analiseImc = 'peso saudável'
+  } else if(this.resultado > 25 && this.resultado <= 29.9 ){
+    this.analiseImc = 'sobrepeso'
+  } else {
+    this.analiseImc = 'obesidade'
+  }
+ }
 }
